1ab4186be76ff4f3cb5651b761116723d29b4c52
Pedro Carrega <pedro.carrega@outlook.com>
2019-12-23 17:08:59 +0000

    starting main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.List;
+
+import com.bezirk.middleware.Bezirk;
+import com.bezirk.middleware.java.proxy.BezirkMiddleware;
+
 public class Main {
+	
+	private static Bezirk bezirk;
+	private static List<String> contacts;
 
 	public static void main(String[] args) {
 		 System.err.println(I18N.getString(Messages.OPTION_MSG));
+		 
+		 BezirkMiddleware.initialize();
+	     bezirk = BezirkMiddleware.registerZirk("Test");
 
 	}
 
