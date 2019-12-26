ce9e80fc2b4256663e5b79d392afb3df99e67bd1
Pedro Carrega <pedro.carrega@outlook.com>
2019-12-26 17:49:47 +0000

    update

--- src/Alertas.aj
+++ src/Alertas.aj
@@ -1,4 +1,9 @@
+import com.bezirk.middleware.java.proxy.BezirkMiddleware;
 
 public aspect Alertas {
 	// TODO Auto-generated aspect
+	before(Hub hub): execution(* *.initialize()) && target(hub) {
+        hub.addEvent(AlertEvent.class);
+    }
+	
 }
--- src/Hub.java
+++ src/Hub.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import com.bezirk.middleware.Bezirk;
 import com.bezirk.middleware.addressing.ZirkEndPoint;
 import com.bezirk.middleware.java.proxy.BezirkMiddleware;
@@ -7,24 +10,37 @@ import com.bezirk.middleware.messages.EventSet;
 public class Hub {
 	
 	private static Reminders reminders;
+	//private static List<String> contacts;
+	private static List <Class<? extends Event>> test = new ArrayList<>();
 	
 	public static void main(String[] args) {
+		
 		reminders = new Reminders();
 		reminders.start();
 		new Hub();
+		System.out.println("test: " + test.size());
 	}
 	
+	
+
 	public Hub() {
+		
 		BezirkMiddleware.initialize();
 		final Bezirk bezirk = BezirkMiddleware.registerZirk("Hub");
 		System.err.println(I18N.getString(Messages.START_HUB));
 		
-		final EventSet setEvents = new EventSet(AlertEvent.class,  MovimentoUpdateEvent.class);//TODO
+		final Class<? extends Event>[] events = getAlerts(test);
+		
+		final EventSet setEvents = new EventSet(events);
+		
+		//final EventSet setEvents = new EventSet(AlertEvent.class,  MovimentoUpdateEvent.class);//TODO
 		
 		setEvents.setEventReceiver(new EventSet.EventReceiver() {
 			
 			@Override
 			public void receiveEvent(Event event, ZirkEndPoint sender) {
+				System.out.println(event.toString());
+				/*
 				//TODO
 				if(event instanceof AlertEvent) {
 					AlertEvent alert = (AlertEvent) event;
@@ -35,12 +51,26 @@ public class Hub {
 				}else if(event instanceof ReminderEvent) {
 					ReminderEvent reminder = (ReminderEvent) event;
 					reminders.addReminder(reminder);
-				}
+				}*/
 				
 			}
 		});
 		
 		bezirk.subscribe(setEvents);
+	}
+
+	public void addEvent(Class<? extends Event> class1) {
+		test.add(class1);
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	private Class<? extends Event>[] getAlerts(List<Class<? extends Event>> test) {
+		Class<? extends Event>[] result = new Class[test.size()];
+		for(int i = 0; i < test.size() ; i++) {
+			result[i] = test.get(i);
+		}
+		return result;
 	}
 
 }
