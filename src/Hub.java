fe18c08d2966a10e23af73c4fe90d233133fb8e9
Pedro Carrega <pedro.carrega@outlook.com>
2019-12-27 02:33:05 +0000

    clean indentation

--- src/Hub.java
+++ src/Hub.java
@@ -9,13 +9,13 @@ import com.bezirk.middleware.messages.Event;
 import com.bezirk.middleware.messages.EventSet;
 
 public class Hub {
-	
+
 	private static Reminders reminders;
 	//private static List<String> contacts;
 	private static List <Class<? extends Event>> test = new ArrayList<>();
-	
+
 	public static void main(String[] args) {
-		
+
 		reminders = new Reminders();
 		reminders.start();
 		new Hub();
@@ -23,19 +23,19 @@ public class Hub {
 
 
 	public Hub() {
-		
+
 		BezirkMiddleware.initialize();
 		final Bezirk bezirk = BezirkMiddleware.registerZirk("Hub");
 		System.err.println(I18N.getString(Messages.START_HUB));
-		
+
 		final Class<? extends Event>[] events = getAlerts(test);
-		
+
 		final EventSet setEvents = new EventSet(events);
-		
+
 		//final EventSet setEvents = new EventSet(AlertEvent.class,  MovimentoUpdateEvent.class);//TODO
-		
+
 		setEvents.setEventReceiver(new EventSet.EventReceiver() {
-			
+
 			@Override
 			public void receiveEvent(Event event, ZirkEndPoint sender) {
 				//in case we need to do specific stuff
@@ -43,9 +43,11 @@ public class Hub {
 					ReminderEvent reminder = (ReminderEvent) event;
 					//do stuff
 				}
-				
+
 				//print event message
 				System.out.println(event.toString());
+
+
 				/*
 				//TODO
 				if(event instanceof AlertEvent) {
@@ -58,18 +60,18 @@ public class Hub {
 					ReminderEvent reminder = (ReminderEvent) event;
 					reminders.addReminder(reminder);
 				}*/
-				
+
 			}
 		});
-		
+
 		bezirk.subscribe(setEvents);
 	}
 
 	public void addEvent(Class<? extends Event> class1) {
 		test.add(class1);
-		
+
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private Class<? extends Event>[] getAlerts(List<Class<? extends Event>> test) {
 		Class<? extends Event>[] result = new Class[test.size()];
@@ -78,51 +80,51 @@ public class Hub {
 		}
 		return result;
 	}
-	
+
 	private static void operations() {
 
-        Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-        while(true) {
-            try {
-                System.out.println("Selecione uma das seguintes possiveis operações:\nAviso\nContacto");
-                String input = sc.nextLine().toLowerCase();
-                if(input.equals("aviso")) {
-                    aviso(sc);
-                }else if(input.equals("contacto")){
-                    contacto(sc);
-                }else {
-                    System.out.println("Tente novamente");
-                }
-            }catch(Exception e) {
-                System.out.println("Inseriu algum dado errado, tente novamente");
-            }
-        }
-    }
+		while(true) {
+			try {
+				System.out.println("Selecione uma das seguintes possiveis operações:\nAviso\nContacto");
+				String input = sc.nextLine().toLowerCase();
+				if(input.equals("aviso")) {
+					aviso(sc);
+				}else if(input.equals("contacto")){
+					contacto(sc);
+				}else {
+					System.out.println("Tente novamente");
+				}
+			}catch(Exception e) {
+				System.out.println("Inseriu algum dado errado, tente novamente");
+			}
+		}
+	}
 
-    private static void contacto(Scanner sc) {
-//        System.out.println("Indique o nome do contacto");
-//        String name = sc.nextLine();
-        System.out.println("Insira o numero telefonico do contacto");
-        String number = sc.nextLine();
+	private static void contacto(Scanner sc) {
+		//        System.out.println("Indique o nome do contacto");
+		//        String name = sc.nextLine();
+		System.out.println("Insira o numero telefonico do contacto");
+		String number = sc.nextLine();
 
-        //contactos.add(number);
+		//contactos.add(number);
 
-    }
+	}
 
-    private static void aviso(Scanner sc) {
+	private static void aviso(Scanner sc) {
 
-        System.out.println("Indique qual a mensagem do aviso");
-        String msg = sc.nextLine();
-        System.out.println("Insira a data a iniciar com o seguinte formato (Ex.2019-10-01|08:00)");
-        String time = sc.nextLine();
-        System.out.println("Insira a data para terminar com o seguinte formato (Ex.2019-10-01|08:00)");
-        String endTime = sc.nextLine();
-        System.out.println("Insira o numero a periodicidade em horas que tem de receber o aviso Ex.8");
-        int timeFrame = sc.nextInt();
+		System.out.println("Indique qual a mensagem do aviso");
+		String msg = sc.nextLine();
+		System.out.println("Insira a data a iniciar com o seguinte formato (Ex.2019-10-01|08:00)");
+		String time = sc.nextLine();
+		System.out.println("Insira a data para terminar com o seguinte formato (Ex.2019-10-01|08:00)");
+		String endTime = sc.nextLine();
+		System.out.println("Insira o numero a periodicidade em horas que tem de receber o aviso Ex.8");
+		int timeFrame = sc.nextInt();
 
-        reminders.addReminder(new ReminderEvent(msg, "112", time, endTime, timeFrame));
+		reminders.addReminder(new ReminderEvent(msg, "112", time, endTime, timeFrame));
 
-    }
+	}
 
 }
